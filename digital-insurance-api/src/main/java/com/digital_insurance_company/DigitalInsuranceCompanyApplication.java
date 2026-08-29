9312a73a03f395c8e0d373693229260354de0263
Laura Garzon <148497204+LauraGarzon28@users.noreply.github.com>
2026-08-29 10:04:14 -0500

    chore: initialize base project structure

---
+++ digital-insurance-api/src/main/java/com/digital_insurance_company/DigitalInsuranceCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.digital_insurance_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalInsuranceCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalInsuranceCompanyApplication.class, args);
+	}
+
+}
---
+++ digital-insurance-api/src/test/java/com/digital_insurance_company/DigitalInsuranceCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_insurance_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalInsuranceCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
